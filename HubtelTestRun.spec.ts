81ceb83634ec84f599694d519314cdd32ec4778d
Chiir-Deri Pius <99278282+Khartapius@users.noreply.github.com>
2023-11-02 06:12:37 +0000

    Add files via upload

--- HubtelTestRun.spec.ts
+++ HubtelTestRun.spec.ts
@@ -6,26 +6,26 @@ test('has title', async({page}) => {
     await page.goto('https://hubtel.com/');
 
     //Click the request money from anyone.
-    await page.getByRole('link', { name:'Request Money from Anyone'}).click();
+    page.getByRole('link', { name: 'Request Money from Anyone' });
 
     // Expect page to have title.
-    await expect(page).toHaveTitle(/Request Money From Anyone - Hubtel/);
+    const title = await page.$('title');
+    const titleTag = await title?.textContent();
+    expect(titleTag).toContain('Hubtel - Find and pay for everyday essentials');
+
+     //Expect page to have footer.
+     const footer = await page.$('footer');
+     const footerText = await footer?.textContent();
+ 
+     expect(footerText).toContain('©2005 - 2023 Hubtel');
 });
 
-test('has footer', async({page}) => {
-    await page.goto('https://hubtel.com/');
 
-    //Click the request money from anyone.
-    await page.getByRole('link', { name:'Request Money from Anyone'}).click();
-
-    //Expect page to have footer.
-    await expect(page.getByRole('heading', {name:'©2005 - 2023 Hubtel'})).toBeVisible();
-});
 
 
 
 //Charge Customers In-Store testing.
-test('has title', async({page}) => {
+test('title', async({page}) => {
     await page.goto('https://hubtel.com/');
 
     //Click the charge customers in store link.
@@ -33,22 +33,18 @@ test('has title', async({page}) => {
 
     // Expect page to have title.
     await expect(page).toHaveTitle(/Charge Customers In-store - Hubtel/);
-});
 
-test('has footer', async({page}) => {
-    await page.goto('https://hubtel.com/');
-
-    //Click the charge cutomers in store link.
-    await page.getByRole('link', { name:'Charge Customers In-Store'}).click();
-
-    //Expect page to have footer.
-    await expect(page.getByRole('heading', {name:'©2005 - 2023 Hubtel'})).toBeVisible();
+     //Expect page to have footer.
+     const footer = await page.$('footer');
+     const footerText = await footer?.textContent();
+ 
+     expect(footerText).toContain('©2005 - 2023 Hubtel');
 });
 
 
 
     //Integrate online checkout testing.
-test('has title', async({page}) => {
+test('1 has title', async({page}) => {
     await page.goto('https://hubtel.com/');
 
     //Click the Integrate online checkout link.
@@ -56,22 +52,18 @@ test('has title', async({page}) => {
 
     // Expect page to have title.
     await expect(page).toHaveTitle(/Integrate Online Checkout - Hubtel/);
-});
 
-test('has footer', async({page}) => {
-    await page.goto('https://hubtel.com/');
-
-    //Click the Integrate online checkout link.
-    await page.getByRole('link', { name:'Integrate Online Checkout'}).click();
-
-    //Expect page to have footer.
-    await expect(page.getByRole('heading', {name:'©2005 - 2023 Hubtel'})).toBeVisible();
+     //Expect page to have footer.
+     const footer = await page.$('footer');
+     const footerText = await footer?.textContent();
+ 
+     expect(footerText).toContain('©2005 - 2023 Hubtel');
 });
 
 
 
     //RaiseUp for Schools testing.
-test('has title', async({page}) => {
+test('2 has title', async({page}) => {
     await page.goto('https://hubtel.com/');
 
     //Click the RaiseUp for Schools link.
@@ -79,22 +71,18 @@ test('has title', async({page}) => {
 
     // Expect page to have title.
     await expect(page).toHaveTitle(/RaiseUp for Schools - Hubtel/);
-});
 
-test('has footer', async({page}) => {
-    await page.goto('https://hubtel.com/');
-
-    //Click the RaiseUp for schools link.
-    await page.getByRole('link', { name:'RaiseUp for Schools'}).click();
-
-    //Expect page to have footer.
-    await expect(page.getByRole('heading', {name:'©2005 - 2023 Hubtel'})).toBeVisible();
-});
+     //Expect page to have footer.
+     const footer = await page.$('footer');
+     const footerText = await footer?.textContent();
+ 
+     expect(footerText).toContain('©2005 - 2023 Hubtel');
+})
 
 
 
     //Hubtel for hospitals testing.
-test('has title', async({page}) => {
+test('3 has title', async({page}) => {
     await page.goto('https://hubtel.com/');
 
     //Click the hubtel for hospitals link.
@@ -102,22 +90,18 @@ test('has title', async({page}) => {
 
     // Expect page to have title.
     await expect(page).toHaveTitle(/Hubtel for Hospitals/);
-});
 
-test('has footer', async({page}) => {
-    await page.goto('https://hubtel.com/');
-
-    //Click the hubtel for hospitals link.
-    await page.getByRole('link', { name:'Hubtel for Hospitals'}).click();
-
-    //Expect page to have footer.
-    await expect(page.getByRole('heading', {name:'©2005 - 2023 Hubtel'})).toBeVisible();
+     //Expect page to have footer.
+     const footer = await page.$('footer');
+     const footerText = await footer?.textContent();
+ 
+     expect(footerText).toContain('©2005 - 2023 Hubtel');
 });
 
 
 
     //Hubtel for governments testing.
-test('has title', async({page}) => {
+test('4 has title', async({page}) => {
     await page.goto('https://hubtel.com/');
     
     //Click the hubtel for government link.
@@ -125,22 +109,17 @@ test('has title', async({page}) => {
     
     // Expect page to have title.
     await expect(page).toHaveTitle(/Hubtel for Government/);
-});
-    
-test('has footer', async({page}) => {
-    await page.goto('https://hubtel.com/');
-    
-    //Click the hubtel for government link.
-    await page.getByRole('link', { name:'Hubtel for Government'}).click();
-    
-    //Expect page to have footer.
-    await expect(page.getByRole('heading', {name:'©2005 - 2023 Hubtel'})).toBeVisible();
-});
-    
+
+     //Expect page to have footer.
+     const footer = await page.$('footer');
+     const footerText = await footer?.textContent();
+ 
+     expect(footerText).toContain('©2005 - 2023 Hubtel');
+});    
 
 
     //Send SMS testing.
-test('has title', async({page}) => {
+test('5 has title', async({page}) => {
     await page.goto('https://hubtel.com/');
     
     //Click the send SMS link.
@@ -148,22 +127,18 @@ test('has title', async({page}) => {
     
     // Expect page to have title.
     await expect(page).toHaveTitle(/Bulk SMS - Hubtel/);
-});
-    
-test('has footer', async({page}) => {
-    await page.goto('https://hubtel.com/');
-    
-    //Click the send SMS link.
-    await page.getByRole('link', { name:'Send SMS'}).click();
-    
-    //Expect page to have footer.
-    await expect(page.getByRole('heading', {name:'©2005 - 2023 Hubtel'})).toBeVisible();
-});
+
+     //Expect page to have footer.
+     const footer = await page.$('footer');
+     const footerText = await footer?.textContent();
+ 
+     expect(footerText).toContain('©2005 - 2023 Hubtel');
+})
 
 
 
     //Service providers testing.
-test('has title', async({page}) => {
+test('6 has title', async({page}) => {
     await page.goto('https://hubtel.com/');
     
     //Click the services providers link.
@@ -171,22 +146,18 @@ test('has title', async({page}) => {
     
     // Expect page to have title.
     await expect(page).toHaveTitle(/Connect your service to Hubtel/);
-});
-    
-test('has footer', async({page}) => {
-    await page.goto('https://hubtel.com/');
-    
-    //Click the service providers link.
-    await page.getByRole('link', { name:'Service Providers'}).click();
-    
-    //Expect page to have footer.
-    await expect(page.getByRole('heading', {name:'©2005 - 2023 Hubtel'})).toBeVisible();
+
+     //Expect page to have footer.
+     const footer = await page.$('footer');
+     const footerText = await footer?.textContent();
+ 
+     expect(footerText).toContain('©2005 - 2023 Hubtel');
 });
 
 
 
     //Shops testing.
-test('has title', async({page}) => {
+test('7 has title', async({page}) => {
     await page.goto('https://hubtel.com/');
         
     //Click the shops link.
@@ -194,22 +165,18 @@ test('has title', async({page}) => {
         
     // Expect page to have title.
     await expect(page).toHaveTitle(/Become a shop partner | Increase sales - Hubtel/);
-});
-        
-    test('has footer', async({page}) => {
-    await page.goto('https://hubtel.com/');
-        
-    //Click the shops link.
-    await page.getByRole('link', { name:'Shops'}).click();
-        
-    //Expect page to have footer.
-    await expect(page.getByRole('heading', {name:'©2005 - 2023 Hubtel'})).toBeVisible();
+
+     //Expect page to have footer.
+     const footer = await page.$('footer');
+     const footerText = await footer?.textContent();
+ 
+     expect(footerText).toContain('©2005 - 2023 Hubtel');
 });
     
 
 
 //Riders testing.
-test('has title', async({page}) => {
+test('8 has title', async({page}) => {
     await page.goto('https://hubtel.com/');
         
     //Click the riders link.
@@ -217,22 +184,18 @@ test('has title', async({page}) => {
         
     // Expect page to have title.
     await expect(page).toHaveTitle(/Beome a rider | Ride with Hubtel/);
-});
-        
-    test('has footer', async({page}) => {
-    await page.goto('https://hubtel.com/');
-        
-    //Click the Riders link.
-    await page.getByRole('link', { name:'Riders'}).click();
-        
-    //Expect page to have footer.
-    await expect(page.getByRole('heading', {name:'©2005 - 2023 Hubtel'})).toBeVisible();
+
+     //Expect page to have footer.
+     const footer = await page.$('footer');
+     const footerText = await footer?.textContent();
+ 
+     expect(footerText).toContain('©2005 - 2023 Hubtel');
 });
 
 
 
     //Restaurants testing.
-test('has title', async({page}) => {
+test('9 has title', async({page}) => {
     await page.goto('https://hubtel.com/');
         
     //Click the restaurants link.
@@ -240,14 +203,11 @@ test('has title', async({page}) => {
         
     // Expect page to have title.
     await expect(page).toHaveTitle(/Become a restaurant partner | Grow your business - Hubtel/);
+
+     //Expect page to have footer.
+     const footer = await page.$('footer');
+     const footerText = await footer?.textContent();
+ 
+     expect(footerText).toContain('©2005 - 2023 Hubtel');
 });
         
-    test('has footer', async({page}) => {
-    await page.goto('https://hubtel.com/');
-        
-    //Click the restaurants link.
-    await page.getByRole('link', { name:'Restaurants'}).click();
-        
-    //Expect page to have footer.
-    await expect(page.getByRole('heading', {name:'©2005 - 2023 Hubtel'})).toBeVisible();
-});
